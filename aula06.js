d08bfd25b259231b5a7daf2f7827e89dd52bdceb
frossanofree <advcontemporaneo@gmail.com>
2019-11-25 00:01:41 -0300

    first commit

---
+++ aula06.js
@@ -0,0 +1,28 @@
+//condição simples
+console.log('o console funcionou corretamente')
+var vel = 60.5
+console.log(`a velocidade do seu carro é ${vel}`)
+if(vel > 60){
+   console.log('Multado')
+} 
+
+//condição composta
+
+var pais = 'Brasil'
+console.log(`${pais}`)
+if(pais == 'EUA'){
+    console.log('não é brasileiro')
+} else{
+    console.log('você é estrangeiro')
+}
+
+
+
+
+
+
+
+
+
+
+
